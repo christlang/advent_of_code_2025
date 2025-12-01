51832aa03715796e9a9f787bd5f24f386caecb76
cl <cl@synyx.de>
2025-12-02 00:21:58 +0100

    day 01 first step

--- src/main/java/de/cutl/Day01.java
+++ src/main/java/de/cutl/Day01.java
@@ -1,11 +1,68 @@
 package de.cutl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class Day01 {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+
+    enum Direction {
+        L,
+        R;
+
+        static Direction parse(String s) {
+            if (s.equals("L")) {
+                return L;
+            }
+            if (s.equals("R")) {
+                return R;
+            }
+            throw new IllegalArgumentException("invalid direction " + s);
+        }
+    }
+
+    record Rotation(Direction d, int steps) {};
+
+    final List<Rotation> rotations = new ArrayList<>();
+    private int position = 50;
+
+    public Day01(List<String> lines) {
+        for (String line : lines) {
+            if  (line.isEmpty()) {
+                continue;
+            }
+            var d = Direction.parse(line.substring(0, 1));
+            var steps = Integer.parseInt(line.substring(1));
+            this.rotations.add(new Rotation(d, steps));
+        }
+
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int result() {
+        int zeroCounter = 0;
+        for (Rotation rotation : rotations) {
+            if (rotation.d == Direction.R) {
+                position += rotation.steps;
+            } else {
+                position -= rotation.steps;
+            }
+            position %= 100;
+
+            if (position < 0) {
+                position += 100;
+            }
+
+            if (position == 0) {
+                zeroCounter++;
+            }
+        }
+        return zeroCounter;
     }
 }
--- src/test/java/de/cutl/Day01Test.java
+++ src/test/java/de/cutl/Day01Test.java
@@ -2,7 +2,12 @@ package de.cutl;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
@@ -10,7 +15,74 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class Day01Test {
 
     @Test
-    void testApp() {
-        assertTrue( true );
+    void exampleL50() {
+
+        Day01 day01 = new Day01(List.of("L50"));
+
+        assertEquals(1, day01.result());
+    }
+
+    @Test
+    void exampleR50() {
+
+        Day01 day01 = new Day01(List.of("R50"));
+
+        assertEquals(1, day01.result());
+    }
+
+    @Test
+    void exampleSomeSteps() {
+
+        Day01 day01 = new Day01(List.of("R50", "R100"));
+
+        assertEquals(2, day01.result());
+    }
+
+    @Test
+    void position1() {
+
+        Day01 day01 = new Day01(List.of("R50", "R1"));
+
+        day01.result();
+
+        assertEquals(1, day01.getPosition());
+    }
+
+    @Test
+    void positionMinus1() {
+
+        Day01 day01 = new Day01(List.of("R50", "L1"));
+
+        day01.result();
+
+        assertEquals(99, day01.getPosition());
+    }
+
+    @Test
+    void positionMinus101() {
+
+        Day01 day01 = new Day01(List.of("R50", "L201"));
+
+        day01.result();
+
+        assertEquals(99, day01.getPosition());
+    }
+
+    @Test
+    void positionExample() throws IOException {
+        List<String> strings = Files.readAllLines(Path.of("tasks/day01_example.txt"));
+
+        Day01 day01 = new Day01(strings);
+
+        assertEquals(3, day01.result());
+    }
+
+    @Test
+    void positionTask() throws IOException {
+        List<String> strings = Files.readAllLines(Path.of("tasks/day01_input.txt"));
+
+        Day01 day01 = new Day01(strings);
+
+        assertEquals(1023, day01.result());
     }
 }
